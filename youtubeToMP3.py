4ada0a035948c8db167a13f5e2b8e73c2179fb85
Joshua Prila <55035345+AlirpJ@users.noreply.github.com>
2022-04-10 22:47:17 -0500

    Update youtubeToMP3.py

--- youtubeToMP3.py
+++ youtubeToMP3.py
@@ -1,14 +1,20 @@
-# Joshua Prila - last updated August 1, 2021
+# Joshua Prila - last updated April 10, 2022
 # Downloading Youtube videos
 # https://pypi.org/project/pytube/
+# https://pytube.io/en/latest/
 
-from pytube3 import YouTube
+from pytube import YouTube
 
-link = input("Enter the URL of the YouTube Video: \n")
+URL = input("enter video URL: ")
 file = 'defaultDirectory'
 
 print("\nLink is: ",link)
 #YouTube(link).streams.first().download()
 #pl.download_all('/path/to/directory/')
-video = YouTube(link)
-video.streams.first().download(file)
+yt = YouTube(URL)
+#yt.streams.first().download(file)
+try:
+  video = yt.streams.filter(only_audio=True).first()
+  out_file = video.download(output_path = "D:downloads") # Default download path is the folder containing this file
+except:
+  print("error")
